51d70cc0f406204fd0ed4798e56c9391c545fab6
LAPTOP-PDSEBN2M\tlsdm <tlsdml123@gmail.com>
2024-06-28 16:39:49 +0900

    QnA페이지 추가

---
+++ myapp/src/components/pages/QnAPage/QnAPage.jsx
@@ -0,0 +1,163 @@
+import './QnA.css';
+import { useState , useEffect } from 'react';
+import QnAdata from './components/QnAdata';
+import CustomDropdown from './components/customDropdown';
+
+const QnAPage = () => {
+    // =================================================================================================
+    console.log('rendering QnAPage');
+    
+    // =================================================================================================
+    // 데이터 불러오기 파트
+    const userData = localStorage.getItem('userData');
+    const categoryOptions = QnAdata.map((data) => data.category);
+    const savedData = localStorage.getItem('personalQnA');
+    const [dropDownData, setDropDownData] = useState({category:'' , details:''});
+    let [details, setDetails] = useState(['상세 구분 선택']);
+
+
+
+    useEffect(() => {
+        if(userData) {
+            const ParsedUserData = JSON.parse(userData);
+            if(ParsedUserData.email) {
+                document.getElementById('QnA_email').value = ParsedUserData.email;
+            }
+        }
+    } , []);    
+    
+    function QnA_getCategory(selectedOption) {
+        setDropDownData({...dropDownData , category: selectedOption});
+    }
+
+    function QnA_getDetails(selectedOption) {
+        setDropDownData({...dropDownData , details: selectedOption});
+    }
+
+    // =================================================================================================
+    // 데이터 저장 파트
+
+    // 예외 처리
+    const [personalQnA, setPersonalQnA] = useState(()=>{
+        return savedData ? JSON.parse(savedData) : [];
+    });
+
+
+    // 제출 버튼 클릭 시 실행되는 함수
+    const QnA_handleSubmit = (e) => {
+        // e.preventDefault();
+
+        // 값 받기
+        const category = dropDownData; //드롭다운에서 받아온 값 0번째는 유형 1번째는 상세구분
+        const title = document.getElementById('QnA_title').value;
+        const content = document.getElementById('QnA_textarea').value;
+        const email = document.getElementById('QnA_email').value;
+
+        // Create a new QnA object
+        const newQnA = {
+            email,
+            category, //0번째는 유형 1번째는 상세구분
+            title,
+            content
+        };
+
+        // Update the state with the new QnA object
+        setPersonalQnA([...personalQnA, newQnA]);
+
+
+        // Clear the form fields
+        document.getElementById('QnA_title').value = '';
+        document.getElementById('QnA_textarea').value = '';
+    };
+    
+    //로컬에 저장
+    useEffect(() => {
+                        if(personalQnA.length > 0) 
+                        localStorage.setItem('personalQnA', JSON.stringify(personalQnA))
+                    } 
+                , [personalQnA] )
+    
+
+    //================================================================================================
+    //인풋 제어 함수
+
+        // 드롭다운 선택 시 상세 구분 변경
+        useEffect(() => {
+            // 카테고리가 선택되었는지 확인
+            if(dropDownData.category) {
+                // 선택된 카테고리에 해당하는 데이터 찾기
+                const selectedCategoryData = QnAdata.find(data => data.category === dropDownData.category);
+                // 상세 정보 설정
+                if(selectedCategoryData) {
+                    const updatedDetails = ['상세 구분 선택', ...selectedCategoryData.details];
+                    setDetails(updatedDetails);
+                }
+            }
+        }, [dropDownData.category, QnAdata]);
+
+    
+    //================================================================================================ 
+    //유효성 검사 함수
+    function CheckValues(e) {
+        const categoryData = dropDownData;
+        const email = document.getElementById('QnA_email').value;
+        const title = document.getElementById('QnA_title').value;
+        const content = document.getElementById('QnA_textarea').value;
+
+        
+        if(categoryData.category === '' || categoryData.details === '') {
+            e.preventDefault();
+            alert('문의 유형을 선택해주세요');
+            return;
+        }
+        
+        if(email === '' || title === '' || content === '') {
+            e.preventDefault();
+            alert('모든 항목을 입력해주세요');
+            return;
+        } else {
+            QnA_handleSubmit();
+        }
+    }
+
+
+
+    //================================================================================================
+    //렌더링 파트
+
+
+    return (        
+        <div className="QnA_page">
+            <h1>문의하기</h1>
+            <form className='QnA_form'>
+                <div className='QnA_inputs'>
+                    <label className='QnA_label' htmlFor="QnA_name">이메일 주소</label>
+                    <input type="text" id='QnA_email' placeholder='답장받을 이메일 입력' />
+
+                    <label className='QnA_label' htmlFor="QnA_Category_select">문의 유형</label>
+                    <CustomDropdown QnA_getValue={QnA_getCategory} QnAdata={categoryOptions}/>
+
+                    <label className='QnA_label' htmlFor="QnA_Category_select_detail">상세 구분</label>
+                    <CustomDropdown QnA_getValue={QnA_getDetails} QnAdata={details}/>
+
+                    <label className='QnA_label' htmlFor="QnA_title">제목</label>
+                    <input id='QnA_title' type="text" placeholder='40자 이내로 제목을 입력하세요' maxLength='40' />
+
+                    <label htmlFor="QnA_textarea" className='QnA_label'>문의내용</label>
+                    <textarea id='QnA_textarea' placeholder='문의할 내용을 자세히 적어주세요' maxLength='20000'></textarea>
+
+                    <p className='QnA_file_custom_label'>파일첨부</p>
+                    <label htmlFor="QnA_file" className='QnA_file_label'>파일 첨부</label>
+                    <input type="file" id='QnA_file'/>
+
+                </div>
+                <button className='QnA_submitBtn' type="submit" onClick={CheckValues}>문의하기</button>
+            </form>
+        </div>
+    )
+
+};
+
+
+
+export default QnAPage;
---
+++ myapp/src/components/pages/QnAPage/components/customDropdown.jsx
@@ -0,0 +1,139 @@
+import React, { useState , useEffect, useMemo, useRef } from 'react';
+
+
+const CustomDropdown = ({QnA_getValue , QnAdata }) => {
+    const options = QnAdata; // 드롭다운 옵션 예시
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+    const DropdownContainer = useRef(null);
+    let [hidden ,setHidden] = useState(true);
+
+    
+    //input 값 받아오기
+    const handleSelect = (event) => {
+        setSelectedOption(event.target.innerText);
+        showOptions(event);
+    };
+
+    //이전 요소 미선택시 disabled
+    useEffect(() => {
+        // selectedOption 값에 따라 클래스를 조건부로 추가하거나 제거
+        if (options.length < 2) {
+            DropdownContainer.current.classList.add('disabled');
+        } else {
+            DropdownContainer.current.classList.remove('disabled');
+        }
+    }, [selectedOption, options]);
+
+    
+
+
+
+    // 부모 컴포넌트로 값 전달
+    useEffect( ()=> QnA_getValue(selectedOption) , [selectedOption] );
+
+    
+
+
+    // 리스트 여닫기
+    const showOptions = (event) => {
+        const CustomDropdown = event.target.closest('.CustomDropdown');
+        const DropdownOptionsArea = CustomDropdown.querySelector('.DropdownOptionsArea');
+        const CustomDropDownArrows = CustomDropdown.querySelector('.CustomDropdownArrows');
+    
+        if (hidden) {
+            CustomDropDownArrows.style.transform = 'rotate(180deg)';
+            DropdownOptionsArea.style.display = 'block';
+        } else {
+            CustomDropDownArrows.style.transform = 'rotate(0deg)'
+            DropdownOptionsArea.style.display = 'none';
+        } 
+    
+        setHidden(!hidden);
+    }
+
+    return (
+        <>
+            <style>
+                {`
+                .CustomDropdown {
+                    list-style: none;
+                    padding: 0;
+                    margin: 0;
+                    
+                    position: relative;
+                    width: 100%;
+                    background: white;
+                    border: 1px solid rgb(0, 0, 0, 0.5);;
+                    border-radius: 4px;
+                }
+                .CustomDropdown li:first-child {
+                    padding: .4rem;
+                    cursor: pointer;
+                    position: relative;
+                }
+                .DropdownOptionsArea {
+                    display: none;
+                    height: auto;
+                    width: 100%;
+
+                    margin-top: 1px;
+                    position: absolute;
+                    
+                    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+                }
+                .CustomDropdownOptions {
+                    padding: 10px;
+                    cursor: pointer;
+                    background-color: white;
+                    width: 100%;
+                }                    
+                .CustomDropdownOptions:hover {
+                    background-color: rgb(200, 200, 200);
+                }
+                .CustomDropdownArrows {
+                    position: absolute;
+                    
+                    background-image: url('https://cdn-icons-png.flaticon.com/128/9931/9931001.png');
+                    background-size: contain;
+                    background-repeat: no-repeat;
+                    background-position: center;
+
+                    height: 1rem;
+                    width: 1rem;
+
+                    right: 10px;
+                    top: calc((100% - 1rem) / 2);
+                    
+                    transition: transform 0.3s;
+                }    
+                .className='ShowCustomDropdownOptions'{
+                    display: block;
+                } 
+                `}
+            </style>
+
+            <ul className='CustomDropdown' value={selectedOption} ref={DropdownContainer}>
+                <li onClick={showOptions} >
+                    <p>{ 
+                    options.includes(selectedOption) ?
+                    selectedOption : options[0]            
+                    }</p>                  
+                    <span className='CustomDropdownArrows'></span>
+                </li>
+
+                <ul className='DropdownOptionsArea'>
+                    {options.filter((option, index)=> index > 0 ).map((option, index) => (
+                        <li className='CustomDropdownOptions' 
+                        key={index}
+                        onClick={handleSelect}>
+                            {option}
+                        </li>
+                    ))}
+                </ul>
+
+            </ul>
+        </>
+    );
+};
+
+export default CustomDropdown;
