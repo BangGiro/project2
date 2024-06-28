383d6f7b5391317f388c70b1de3394d2985be721
ZENBOOK\10081 <10081028kk16@gmail.com>
2024-06-28 13:20:09 +0900

    더미데이터 추가, 회원추가시 중복회원체크

--- myapp/src/components/management/components/Management.jsx
+++ myapp/src/components/management/components/Management.jsx
@@ -1,40 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import UserList from './UserList';
-// import AddUserModal from './AddUserModal';
-// import './Management.css';
-
-// function Management() {
-//   const [users, setUsers] = useState([]);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   useEffect(() => {
-//     const storedMemos = JSON.parse(localStorage.getItem('memos')) || [];
-//     setUsers(storedMemos);
-//   }, []);
-
-//   const handleAddUser = (user, memo) => {
-//     const newUser = { ...user, memo };
-//     const updatedUsers = [...users, newUser];
-//     setUsers(updatedUsers);
-//     localStorage.setItem('memos', JSON.stringify(updatedUsers));
-//   };
-
-//   return (
-//     <div className='mainmanagement'>
-//       <button onClick={() => setIsModalOpen(true)}>회원 추가</button>
-//       <UserList users={users} />
-//       {isModalOpen && (
-//         <AddUserModal
-//           onClose={() => setIsModalOpen(false)}
-//           onAddUser={handleAddUser}
-//         />
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Management;
-
 import React, { useState, useEffect } from 'react';
 import UserList from './UserList';
 import AddUserModal from './AddUserModal';
@@ -67,6 +30,7 @@ function Management({ loggedInEmail }) {
           <AddUserModal
             onClose={() => setIsModalOpen(false)}
             onAddUser={handleAddUser}
+            existingUsers={users} // 기존 회원 목록 전달
           />
         )}
       </div>
