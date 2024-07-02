00ded02df270b4408929c95a4f162f281c9d770c
ZENBOOK\10081 <10081028kk16@gmail.com>
2024-07-02 12:55:03 +0900

    뭔가 ㅈ댄거같으니 저장

--- myapp/src/components/management/components/UserItem.jsx
+++ myapp/src/components/management/components/UserItem.jsx
@@ -4,10 +4,10 @@ import './UserItem.css';
 function UserItem({ user }) {
     return (
         <div className="managementuseritem">
+            <img src="/image/profile.webp" alt="프로필사진" />
             <h3>{user.name}</h3>
             <p>{user.email}</p>
             <p>{user.gender}</p>
-            <p>{user.memo}</p>
         </div>
     );
 }
