9e0ad6b788f2e0844bf2c47abd627bdbbf2a665f
ZENBOOK\10081 <10081028kk16@gmail.com>
2024-07-04 17:59:36 +0900

    no message

--- myapp/src/components/PrivateRoute.jsx
+++ myapp/src/components/PrivateRoute.jsx
@@ -5,10 +5,21 @@ import { Navigate } from 'react-router-dom';
 import { isLoggedIn } from '../helpers/auth';
 
 const PrivateRoute = ({ element }) => {
-  
-  return isLoggedIn() ? element : 
-    <Navigate to="/login" />,
-    alert('로그인이 필요한 페이지 입니다.');
+    if(isLoggedIn()) {
+      return element;
+    }
+    else {
+      return(
+        alert('로그인이 필요한 페이지 입니다.'),
+        <Navigate to="/login" />
+      ) 
+    }
+
+  // return (
+  // isLoggedIn() ? element : 
+  //   <Navigate to="/login" />
+  //   alert('로그인이 필요한 페이지 입니다.');
+  // )  
 };
 
 export default PrivateRoute;
