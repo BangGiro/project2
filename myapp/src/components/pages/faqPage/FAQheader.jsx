6d4088fc2ccd4ac5e62b3bd847409ce4f393fc65
LAPTOP-PDSEBN2M\tlsdm <tlsdml123@gmail.com>
2024-07-04 11:13:19 +0900

    faq페이지 css수정

--- myapp/src/components/pages/faqPage/FAQheader.jsx
+++ myapp/src/components/pages/faqPage/FAQheader.jsx
@@ -2,9 +2,10 @@
 const FAQheader = function() {
     return(
         <div className="FAQheader">
+            <img src="/image/unDrawSVG/undraw_faq.svg" alt="" />
             <h1 className="FAQtitle">자주 묻는 질문</h1>
         </div>
     )
 };
 
-export default FAQheader;
+export default FAQheader
--- myapp/src/components/pages/faqPage/FAQsearch.jsx
+++ myapp/src/components/pages/faqPage/FAQsearch.jsx
@@ -7,7 +7,7 @@ const FAQsearch = function({categoryOut , detectSearch }) {
     return(
         <div className="FAQsearchContainer" >
             <select className="FAQselect" onChange={categoryOut} >
-                <option value="all" key="">카테고리 전체</option>
+                <option value="all" key="">전체</option>
                 <option value="서비스" key="">서비스</option>
                 <option value="결제" key="">결제</option>
                 <option value="환불" key="">환불</option>
