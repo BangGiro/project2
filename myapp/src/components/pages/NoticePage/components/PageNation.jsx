4f2dfe9003a63822a9c232d24de727e6973b918f
LAPTOP-PDSEBN2M\tlsdm <tlsdml123@gmail.com>
2024-07-05 18:35:57 +0900

    피드백 반영 (페이지 네이션 현재 페이지 표시기능 및 페이지 네이션 컴포넌트화)

--- myapp/src/components/pages/NoticePage/components/PageNation.jsx
+++ myapp/src/components/pages/NoticePage/components/PageNation.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useRef } from "react";
 
 export default function PageNation({ currentList, noticesPerPage, paginate }) {
+    let previousPage = useRef(null) ;
 
-    let previousPage;
-    
+
+    useEffect(() => {
+        previousPage.current = document.querySelector('.paginateBTN');
+        if(previousPage.current) {
+            previousPage.current.classList.add('PageNationActive');
+        }
+    }
+    ,[]);
+
+    function PageEvent(e , index) {
+        if (e.target !== previousPage.current) {
+            e.target.classList.add('PageNationActive');
+            previousPage.current.classList.remove('PageNationActive');
+        } 
+        paginate(index + 1);
+        previousPage.current = e.target;
+    }
 
     return (
         <div className="pagination">
         {Array.from({ length: Math.ceil(currentList.length / noticesPerPage) }, (_, index) => (
-            <button key={index} onClick={() => paginate(index + 1)}>
+            <button className="paginateBTN" key={index} onClick={(e) => PageEvent( e, index )}>
                 {index + 1}
             </button> 
         ))}
