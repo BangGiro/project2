544b405f33f47d3a02cc8ecf29816ce9213b87db
LAPTOP-PDSEBN2M\tlsdm <tlsdml123@gmail.com>
2024-10-23 15:09:16 +0900

    다시 병합 정상화 시도

--- myapp/src/components/pages/login/SignUp.jsx
+++ myapp/src/components/pages/login/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Login.css";
 import { apiCall } from "../../../service/apiService";
 
@@ -12,7 +12,8 @@ export default function SignUp() {
         loginType: "",
         joinDate: ""
     });
-
+    const location = useLocation();
+    const isAdd = location.state;
     const [errorMessage, setErrorMessage] = useState("");
     const [duplicateMessage, setDuplicateMessage] = useState("");
     const [signZipCode, setZipCode] = useState("");
